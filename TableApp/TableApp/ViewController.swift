1a669b3900f1c2a20dbf826b18ea8d9370083589
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-12-02 13:50:55 +0300

    Project created

---
+++ TableApp/TableApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TableApp
+//
+//  Created by comp on 2.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
